874a8960d166275293d7e836f1367b4c53e04859
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2024-12-16 15:49:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // your code here
+function updateUrl(){
+	const name = document.getElementById('name').value;
+	const year = document.getElementById('year').value;
+
+	const newURL = `https://localhost:8080/?name=${encodeURIComponent(name)}&year=${encodeURIComponent(year) } `
+
+	document.getElementById('url').textContent = newURL;
+}
